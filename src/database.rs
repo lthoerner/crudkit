e46795b6fd952a0c676c6699b65cf5d581dba9c6
Rory Sullivan <codingrory@gmail.com>
2025-02-12 19:57:48 -0800

    feat: add write traits and associated items

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,18 @@
+use sqlx::{query_builder::QueryBuilder, Postgres};
+
+pub const SQL_PARAMETER_BIND_LIMIT: usize = u16::MAX as usize;
+
 #[derive(Clone)]
 pub struct Database {
     pub connection: sqlx::PgPool,
+}
+
+impl Database {
+    pub async fn execute_query_builder<'a>(&self, mut query_builder: QueryBuilder<'a, Postgres>) {
+        query_builder
+            .build()
+            .execute(&self.connection)
+            .await
+            .unwrap();
+    }
 }
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,3 +1,4 @@
 pub mod id_parameter;
 pub mod read;
 pub mod shared;
+pub mod write;
