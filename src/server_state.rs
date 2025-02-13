3f078dbfe552516d78ebce2d74116d01a89f067f
lthoerner <lowellashton@gmail.com>
2025-02-13 15:43:46 -0800

    fix: remove unnecessary field from `ServerState` that was included from the original repo

--- src/server_state.rs
+++ src/server_state.rs
@@ -1,7 +1,7 @@
 use crate::database::Database;
 
+// TODO: This will need to be replaced with a generic
 #[derive(Clone)]
 pub struct ServerState {
     pub database: Database,
-    imei_info_api_key: String,
 }
