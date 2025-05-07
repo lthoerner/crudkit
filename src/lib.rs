9c93e8f7ff1a9fb8148710488e4da9a2085dbf6b
lthoerner <lowellashton@gmail.com>
2025-05-07 15:55:27 -0700

    feat: add prelude including all traits and derive macros

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,14 @@ pub use axum;
 pub use crudkit_derive::*;
 pub use http;
 pub use log;
+
+pub mod prelude {
+    pub use super::traits::id_parameter::{GenericIdParameter, IdParameter};
+    pub use super::traits::read::{ReadRecord, ReadRelation};
+    pub use super::traits::shared::{IdentifiableRecord, Record, Relation};
+    pub use super::traits::write::{BulkInsert, SingleInsert, WriteRecord, WriteRelation};
+    pub use crudkit_derive::IdParameter;
+    pub use crudkit_derive::{BulkInsert, SingleInsert, WriteRecord, WriteRelation};
+    pub use crudkit_derive::{IdentifiableRecord, Record, Relation};
+    pub use crudkit_derive::{ReadRecord, ReadRelation};
+}
